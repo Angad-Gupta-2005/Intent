0bb16117da7f2ee879e67a61b479f6ca892d68e4
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-10-29 13:48:36 +0530

    Implicit Intent is done

--- app/src/main/java/com/angad/intent/MainActivity.kt
+++ app/src/main/java/com/angad/intent/MainActivity.kt
@@ -1,20 +1,90 @@
 package com.angad.intent
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.angad.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //    Creating instance of view binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+//        Initialised binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+//        On click open web page button to Open web page
+        onClickWebPageButton()
+
+//        On click open phone call button to open phone call
+        onClickPhoneCallButton()
+
+//        On click open camera button to open camera
+        onCLickOpenCameraButton()
+
+//        On click share message button to share message with various messaging app
+        onClickShareMessageButton()
+    }
+
+//    Function that open web page or application of provided link
+    private fun onClickWebPageButton() {
+        binding.openWebPage.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.youtube.com/watch?v=7AckvzGj_vI&list=PLUhfM8afLE_NQbVaoIEhceR9npbY57Pdg&index=60")
+            )
+            startActivity(intent)
+        }
+    }
+
+//    Function that open phone call
+    private fun onClickPhoneCallButton() {
+        binding.openPhoneCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel: 8400175672")
+            startActivity(intent)
+        }
+    }
+
+//    Function that open camera to click photo
+    private fun onCLickOpenCameraButton() {
+        binding.openCamera.setOnClickListener {
+            //    For open camera or photo, video we use MediaStone
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+    }
+
+//    Function that share the message
+    private fun onClickShareMessageButton() {
+        binding.shareMessage.setOnClickListener {
+            //    Getting message from editText
+            val message = binding.message.text.toString()
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            if (message.isEmpty()){
+                Toast.makeText(this, "Please enter the message", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                startActivity(Intent.createChooser(intent, "Share Via"))
+            }
         }
     }
 }
